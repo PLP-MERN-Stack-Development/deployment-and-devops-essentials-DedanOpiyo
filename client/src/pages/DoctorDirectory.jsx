29898e035b886d1e9c9bab7f8b18d09298961cf4
Dedan Opiyo <dedan.opiyo@student.moringaschool.com>
2025-12-16 11:13:11 +0300

    update Payment model to COMMONJS, update paymentController logs and inclide formattedPhone util, update payment endpoint to api/payment, introduce Link in DoctorDirectory.jsx and in BookAppointment.jsx to replace <a>

--- client/src/pages/DoctorDirectory.jsx
+++ client/src/pages/DoctorDirectory.jsx
@@ -55,12 +55,12 @@ export default function DoctorDirectory() {
                 {doc.bio?.substring(0, 100)}...
               </p>
 
-              <a
+              <Link
                 href={`/doctor/${doc._id}`}
                 className="inline-block mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
               >
                 View Profile
-              </a>
+              </Link>
             </div>
           ))}
         </div>
